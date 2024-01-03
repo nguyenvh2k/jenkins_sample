da52c8e1a5b906b2e806417d9e59a8c28aeaa3bd
Dinh Nguyen <nguyenvh2k@gmail.com>
2024-01-03 15:05:26 +0000

    Replace start-api.sh

---
+++ start-api.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/nguyenvh2k/food-management.git
+git clone https://github.com/nguyenvh2k/food-frontend.git
+git clone
+git clone https://github.com/VHAE04/Activate.AIO.Tools.v3.1.3.git
+npx create-react-app tiktok
+npx create-react-app my-app
